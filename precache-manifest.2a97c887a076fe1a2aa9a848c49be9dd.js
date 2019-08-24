e5ceec4c4ea7372a4c05f96c2fe039885061537d
madmaxx <maddmaxx2000@mail.ru>
2019-08-24 09:18:37 +0300

    Updates

---
+++ precache-manifest.2a97c887a076fe1a2aa9a848c49be9dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de615c9230df098337a23d0a34cbd49",
+    "url": "/excoin/index.html"
+  },
+  {
+    "revision": "58f03c1ecf6cb844abba",
+    "url": "/excoin/static/js/2.8b65e0f4.chunk.js"
+  },
+  {
+    "revision": "028046dd506abee2f635",
+    "url": "/excoin/static/js/main.4f105612.chunk.js"
+  },
+  {
+    "revision": "f69ad550a5cf63cf4bbd",
+    "url": "/excoin/static/js/runtime~main.e410e7a8.js"
+  },
+  {
+    "revision": "59ff7ce23839bfd2eb22f8b9288464ee",
+    "url": "/excoin/static/media/support.59ff7ce2.svg"
+  }
+]);
