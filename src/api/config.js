bab1061fb3f5cb269d9f2d3fb58e726a4c10d2c5
madmaxx <maddmaxx2000@mail.ru>
2019-10-27 10:02:16 +0300

    update npm package

---
+++ src/api/config.js
@@ -0,0 +1,8 @@
+export default {
+  login: 'login',
+  fetchData: source => `${source}/analyze`,
+  fetchDataExport: source => `${source}/export`,
+  userMe: 'user/me',
+  logout: 'logout',
+  metadata: 'metadata',
+}
